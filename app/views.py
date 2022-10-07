3761c0416681269f0984d3f88626d4f75c84b3e3
infinity_pain <94642389+slipperss@users.noreply.github.com>
2022-10-07 19:38:27 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -11,11 +11,7 @@ class UserProfileListView(ListAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        user = self.request.user
-        serializer.save(user=user)
-
+    
 
 class UserProfileDetailView(RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
